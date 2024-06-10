d7a0b40a46e96340a633de47c7239b294c6fa88d
Mirgo25 <mirgo2513@gmail.com>
2024-06-11 01:41:48 +0300

    check env log

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,5 @@
 export function getEnvPath(): string {
+  console.log({ env: process.env.NODE_ENV });
   if (process.env.NODE_ENV === 'development') return '.env.development';
   if (process.env.NODE_ENV === 'test') return '.env.test';
   if (process.env.NODE_ENV === 'production') return '.env.production';
