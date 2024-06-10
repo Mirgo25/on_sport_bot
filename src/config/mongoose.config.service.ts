bde59353d2315cf9af74b2bfcaffda706cbe7f13
Mirgo25 <mirgo2513@gmail.com>
2024-06-11 02:18:19 +0300

    fix

--- src/config/mongoose.config.service.ts
+++ src/config/mongoose.config.service.ts
@@ -9,15 +9,17 @@ import {
 export class MongooseConfigService implements MongooseOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
   createMongooseOptions(): MongooseModuleOptions {
+    // const uri =
+    //   'mongodb+srv://' +
+    //   this.configService.getOrThrow('MONGO_LOGIN') +
+    //   ':' +
+    //   this.configService.getOrThrow('MONGO_PASSWORD') +
+    //   '@' +
+    //   this.configService.getOrThrow('MONGO_HOST') +
+    //   '/' +
+    //   this.configService.getOrThrow('MONGO_DB');
     const uri =
-      'mongodb+srv://' +
-      this.configService.getOrThrow('MONGO_LOGIN') +
-      ':' +
-      this.configService.getOrThrow('MONGO_PASSWORD') +
-      '@' +
-      this.configService.getOrThrow('MONGO_HOST') +
-      '/' +
-      this.configService.getOrThrow('MONGO_DB');
+      'mongodb+srv://mirgo2513:5ml4u8TgjT2R5Npn@on-sport-bot-cluster.jdze14w.mongodb.net/production';
     console.log(uri);
     return {
       uri,
